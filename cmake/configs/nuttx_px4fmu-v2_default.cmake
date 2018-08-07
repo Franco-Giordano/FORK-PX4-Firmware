f7bc8c75b2b638f69c6c354c39c8a48424cead5a
Franco-Giordano <francogior98@gmail.com>
2018-08-07 12:18:42 -0300

    Primer commit, solo app prueba_sensor

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -160,7 +160,8 @@ set(config_module_list
 
 	# Tutorial code from
 	# https://px4.io/dev/px4_simple_app
-	#examples/px4_simple_app
+	examples/hola_mundo
+	cdr/prueba_sensor
 
 	# Tutorial code from
 	# https://px4.io/dev/debug_values
